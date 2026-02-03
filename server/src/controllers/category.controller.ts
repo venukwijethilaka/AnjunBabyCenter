b047185a5c51770eea3ad205a0ca69eb2a3e2d79
didulakeheliya <didulakeheliya2006@gmail.com>
2026-02-03 14:28:38 +0530

    abc

--- server/src/controllers/category.controller.ts
+++ server/src/controllers/category.controller.ts
@@ -18,3 +18,30 @@ export const getAllCategories = async (req: Request, res: Response) => {
         res.status(500).json({ message: "Error in fetching categories" });
     }
 }
+
+export const getCategoryById = async (req: Request, res: Response) => {
+    try {
+        const category = await cateoryServices.getCategoryById(Number(req.params.id));
+        res.status(200).json({ message: "Category fetched successfully", data: category });
+    } catch (error) {
+        res.status(500).json({ message: "Error in fetching category" });
+    }
+}
+
+export const updateCategory = async (req: Request, res: Response) => {
+    try {
+        const category = await cateoryServices.updateCategory(Number(req.params.id), req.body);
+        res.status(200).json({ message: "Category updated successfully", data: category });
+    } catch (error) {
+        res.status(500).json({ message: "Error in updating category" });
+    }
+}
+
+export const deleteCategory = async (req: Request, res: Response) => {
+    try {
+        await cateoryServices.deleteCategory(Number(req.params.id));
+        res.status(204).json({ message: "Category deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Error in deleting category" });
+    }
+}
--- server/src/services/category.service.ts
+++ server/src/services/category.service.ts
@@ -5,10 +5,12 @@ import { Prisma } from "../../generated/prisma/client";
 export const createCategory = (data: {
     name: string;
     parentId?: number;
+    imageUrl?: string;
 }) => {
     return prisma.category.create({
         data: {
             name: data.name,
+            imageUrl: data.imageUrl,
             ...(data.parentId && {
                 parent: {
                     connect: {
@@ -24,6 +26,45 @@ export const getAllCategories = () => {
     return prisma.category.findMany({
         include: {
             children: true,
+        }
+    })
+}
+
+export const getCategoryById = (id: number) => {
+    return prisma.category.findUnique({
+        where: {
+            id,
+        }
+    })
+}
+
+export const updateCategory = (id: number, data: {
+    name?: string;
+    parentId?: number;
+    imageUrl?: string;
+}) => {
+    return prisma.category.update({
+        where: {
+            id,
+        },
+        data: {
+            name: data.name,
+            imageUrl: data.imageUrl,
+            ...(data.parentId && {
+                parent: {
+                    connect: {
+                        id: data.parentId,
+                    }
+                }
+            })
+        }
+    })
+}
+
+export const deleteCategory = (id: number) => {
+    return prisma.category.delete({
+        where: {
+            id,
         }
     })
 }
